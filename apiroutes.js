1da1abc2ae27f9d288df37a9970ccc86ec009277
Shayna Murphy <110637697+Shaynam1977@users.noreply.github.com>
2022-12-16 16:06:58 -0800

    add api routes

---
+++ apiroutes.js
@@ -0,0 +1,27 @@
+const router= require("express").Router()
+const fs=require("fs")
+let store=require("./db/db.json")
+
+router.get('/notes', (req, res) =>
+ res.json(store)
+);
+
+router.post('/notes', (req, res) =>
+ { let newNoteModel={
+    id: Math.random(),
+    title: req.body.title,
+    text: req.body.text
+};
+store.push(newNoteModel);
+fs.writeFileSync("./db/db.json", JSON.stringify(store), function(err, res) {
+    if (err) {
+        throw err;
+    }
+});
+res.json(store);
+
+ }
+);
+
+module.exports=router
+
